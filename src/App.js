0585b88cdc783ea3d20aa926454ea0b42bee909a
Slice Wess <ernestwesson86@gmail.com>
2020-12-07 19:54:20 -0800

    working on app.js creating state function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import React, {Component} from "react";
 import './App.css';
+import Score from "./component/Score";
+import Wrapper from "./component/Wrapper";
+import fruits from "./fruits.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+  state = {
+    fruits: fruits,
+    clickedImgId: [],
+    score: 0,
+    goal: 12,
+    status: ""
+  };
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header"/>
+          <h1>
+            Fruit Game
+          </h1>
+          </div>
+
+    )
+    }
+
 }
-
 export default App;
